53c9eda4a3fbb9a27593d9fc471f71f81fcdc528
Quinn Elder <qre.turing@gmail.com>
2020-03-30 15:55:54 -0600

    Add functionality for the Favorite recipes button

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,15 @@
 const populatePage = document.querySelector('.populate-recipes');
-const filterFavoriteRecipes = document.querySelector('.favorites-btn');
-const filterCookbook = document.querySelector('.cookbook-btn');
+const navBtns = document.querySelector('.header-btns');
 const mainSearchInput = document.querySelector('.search-all');
 const recipeTagContainer = document.querySelector('.tag-container');
 // const displayRecipeCard = document.querySelector('.display-recipe');
 // const ingredientsList = document.querySelector('.ingredients-list');
 const playBTN = document.getElementById('play')
 
+navBtns.addEventListener("click", function() {
+displayFavRecipes(event);
+displayCookbookRecipes(event);
+});
 mainSearchInput.addEventListener('input', searchRecipes);
 recipeTagContainer.addEventListener('click', filterByTag)
 populatePage.addEventListener('click', function() {
@@ -228,18 +231,34 @@ function filterByTag(event) {
   let currentTag = event.target
   let tagId = event.target.id
   if (currentTag.classList.contains('tag-img')) {
-    displayRecipesByTag(tagId, currentTag);
+    displayRecipesByTag(tagId);
   }
 }
 
-function displayRecipesByTag(tagId, currentTag) {
+function displayRecipesByTag(tagId) {
   let allCards = document.querySelectorAll('.recipe-card');
   allCards.forEach(card => card.classList.remove('hidden'));
 
   recipeData.forEach(recipe => {
     let currentRecipe = document.querySelector(`.recipe-card[data-id="${recipe.id}"]`)
-    if(!recipe.tags.includes(tagId)) {
+    if (!recipe.tags.includes(tagId)) {
       currentRecipe.classList.add('hidden');
     }
   });
+}
+
+function displayFavRecipes(event) {
+let allRecipes = document.querySelectorAll(".recipe-card")
+allRecipes.forEach(recipe => recipe.classList.add('hidden'));
+
+if (event.target.classList.contains("favorites-btn")) {
+  user.favRecipes.forEach((recipe) => {
+    let currentRecipe = document.querySelector(`.recipe-card[data-id="${recipe.id}"]`)
+    currentRecipe.classList.remove('hidden')
+  });
+}
+
+}
+function displayCookbookRecipes(event) {
+
 }
